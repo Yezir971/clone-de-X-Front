602cc34516a465135383dec68bcb83e1acf62a66
MathPrm <primotmathilde@gmail.com>
2025-02-11 10:02:26 +0100

    feat(search): add redirection link

--- app/components/sideBar.js
+++ app/components/sideBar.js
@@ -50,8 +50,8 @@ const SideBar = ({hide}) => {
                             </Link>
                         </li>
                         <li>
-                            <Link href={'/user-page'} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <MdPersonSearch className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white w-6 h-6" />
+                            <Link href={'/search'} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <MdPersonSearch className="shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white w-6 h-6" />
                                 <span className="flex-1 ms-3 whitespace-nowrap">Rechercher</span>
                             </Link>
                         </li>
